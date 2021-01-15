51939508f9ce68cbffc60d59c658496b96d4a50c
Brian <93.brian.e.marin@gmail.com>
2021-01-15 23:19:12 +0100

    Return classified user book interests by username

--- cmd/something/backend/controller/users/UsersGetController.go
+++ cmd/something/backend/controller/users/UsersGetController.go
@@ -2,13 +2,15 @@ package users
 
 import (
 	"net/http"
+
+	bookFinder "something/internal/books/application/find"
 	"something/internal/users/application/find"
 
 	"github.com/gin-gonic/gin"
 )
 
 // GetUsersController ...
-func GetUsersController(finder find.Service) func(c *gin.Context) {
+func GetUsersController(finder find.Service, bFinder bookFinder.Service) func(c *gin.Context) {
 	return func(c *gin.Context) {
 		username := c.Query("username")
 		if username != "" {
@@ -25,8 +27,10 @@ func GetUsersController(finder find.Service) func(c *gin.Context) {
 				})
 				return
 			}
+			interests := classifyBookInterests(user.Interests, bFinder)
 			c.JSON(http.StatusOK, gin.H{
-				"data": user,
+				"data":      user,
+				"interests": interests,
 			})
 			return
 		}
@@ -43,4 +47,50 @@ func GetUsersController(finder find.Service) func(c *gin.Context) {
 		})
 		return
 	}
+}
+
+// TODO Refactor code below to classify user book interests
+
+type bookShort struct {
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+}
+
+func classifyBookInterests(interests map[string]string, finder bookFinder.Service) map[string][]*bookShort {
+
+	bookInterests := map[string][]*bookShort{}
+
+	pending := []*bookShort{}
+	reading := []*bookShort{}
+	done := []*bookShort{}
+
+	for bookID, status := range interests {
+		book := &bookShort{}
+		bookResponse, err := finder.FindBookByID(bookID)
+		if err != nil {
+			continue
+		}
+		book.ID = bookResponse.ID
+		book.Title = bookResponse.Title
+		book.Author = bookResponse.Author
+
+		switch status {
+		case "pending":
+			pending = append(pending, book)
+			break
+		case "reading":
+			reading = append(reading, book)
+			break
+		case "done":
+			done = append(done, book)
+			break
+		}
+	}
+
+	bookInterests["pending"] = pending
+	bookInterests["reading"] = reading
+	bookInterests["done"] = done
+
+	return bookInterests
 }
